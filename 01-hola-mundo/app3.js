86ca50517c8b76bf7405e66a86abfc4db847e940
williamsggiraldo <ggiraldow@gmail.com>
2021-10-26 20:44:41 -0500

    se agregan comentarios

--- 01-hola-mundo/app3.js
+++ 01-hola-mundo/app3.js
@@ -1,16 +1,20 @@
-
+// Se ejecuta 1
 console.log('Inicio del programa');
 
+// Se ejecuta 5
 setTimeout( function() {
     console.log('Primer timeout');
 }, 3000);
 
+// Se ejecuta 3
 setTimeout( function() {
     console.log('Segundo timeout');
 }, 0);
 
+// Se ejecuta 4
 setTimeout( function() {
     console.log('Tercer timeout');
 }, 0);
 
+// Se ejecuta 2
 console.log('Fin del programa');
